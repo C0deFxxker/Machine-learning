352a9f654adae5f554eedba8d83abf086ac30d4b
MagicBlocks <502291755@qq.com>
2017-01-22 02:27:55 +0800

    k邻近算法做手写数字识别

---
+++ kNN/handwritten_digit.py
@@ -0,0 +1,47 @@
+import kNN
+from numpy import *
+
+def load_data(filepath):
+    file = open(filepath)
+    data_set = []
+    data_set_result = []
+    for each_line in file:
+        each_line = each_line.rstrip()
+        elems = each_line.split(",")
+        data_set.append(elems[:-1])
+        data_set_result.append(int(elems[-1]))
+    file.close()
+    return data_set, data_set_result
+
+def create_train_data_set():
+    return load_data("optdigits.tra")
+
+def create_test_data_set():
+    return load_data("optdigits.tes")
+
+(train_data_set, train_data_set_result) = create_train_data_set()
+(test_data_set, test_result) = create_test_data_set()
+
+train_count = len(train_data_set)
+test_count = len(test_data_set)
+data_set = zeros((train_count+test_count, 64))
+data_set_result = []
+for i in range(train_count):
+    data_set[i] = train_data_set[i]
+for i in range(test_count):
+    data_set[i+train_count] = test_data_set[i]
+
+data_set_result.extend(train_data_set_result)
+data_set_result.extend(test_result)
+
+success_count = 0
+for i in range(test_count):
+    # k近邻算法（输入向量，训练样本集，标签向量，最近邻居的数目）
+    test = kNN.classify0(mat(test_data_set[i]).astype("float64"), mat(train_data_set).astype("float64"), train_data_set_result, 2)
+    if test != test_result[i]:
+        print("第%d次测试：结果错误，测试结果为%d，正确答案是%d\n\t测试数：%d, 正确数：%d, 失败数：%d，正确率：%f" \
+              % (i + 1, test, test_result[i], i + 1, success_count, (i+1)-success_count, success_count/(i+1)))
+    else:
+        success_count += 1
+        print("第%d次测试：结果正确，测试结果为%d\n\t测试数：%d, 正确数：%d, 失败数：%d，正确率：%f" \
+              % (i + 1, test, i + 1, success_count, (i + 1) - success_count, success_count / (i + 1)))
---
+++ kNN/kNN.py
@@ -0,0 +1,40 @@
+from numpy import *
+import operator
+
+# k近邻算法（输入向量，训练样本集，标签向量，最近邻居的数目）
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    # 距离计算
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = square(diffMat)
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqrt(sqDistances)
+    arrDistances = zeros((distances.shape[0]))
+    for i in range(distances.shape[0]):
+        arrDistances[i] = distances[i][0]
+    # 将距离计算结果排序
+    sortedDistindicies = argsort(arrDistances)
+
+    # 统计前k个结果中，各个分类的出现频率
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistindicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+
+    # 将分类按出现频率降序排序
+    sortedClassCount = sorted(classCount.items(),
+                              key=operator.itemgetter(1), reverse=True)
+
+    # 频率出现最高的分类，就认为是预测结果
+    return sortedClassCount[0][0]
+
+# 归一化数据（矩阵）
+def autoNorm(dataSet:matrix):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet / tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
